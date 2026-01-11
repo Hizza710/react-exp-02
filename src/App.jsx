c1fbe67e7478786362d9aab022653f36bea533da
hizza1204-hue <hizza1204@gmail.com>
2026-01-11 15:42:06 +0900

    リアクト中級一式

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>💫リアクト中級編、よろしくお願いします💫</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+           <code> react-exp-02-ri5uf0uyn-hizza710s-projects.vercel.app　</code> 
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        やっと初級編の理解ができました　よろしくお願いいたします！
       </p>
     </>
   )
